44682dc7ecd3071afdd56c7bc365970cd3a97176
PavanCodes05 <pavancodes05@gmail.com>
2025-04-17 18:10:27 +0530

    Add FindLanguage Functionality

--- src/extension.ts
+++ src/extension.ts
@@ -1,17 +1,25 @@
 import * as vscode from 'vscode';
 
+import { identifyLanguage } from './utils/index.js';
+
 export function activate(context: vscode.ExtensionContext) {
 	console.log("NeuroCode is Activated");
 
 	const explain = vscode.commands.registerCommand('neurocode.explain', () => {
+		const lang = identifyLanguage();
+
 		vscode.window.showInformationMessage("Explain Code Snippet");
 	});
 
 	const refactor = vscode.commands.registerCommand('neurocode.refactor', () => {
+		const lang = identifyLanguage();
+
 		vscode.window.showInformationMessage("Refactor Code Snippet");
 	});
 
 	const generateDoc = vscode.commands.registerCommand('neurocode.generateDoc', () => {
+		const lang = identifyLanguage();
+
 		vscode.window.showInformationMessage("Generate Doc");
 	});
 
---
+++ src/utils/index.ts
@@ -0,0 +1,3 @@
+import { identifyLanguage } from './fileLanguageUtils.js';
+
+export { identifyLanguage };
