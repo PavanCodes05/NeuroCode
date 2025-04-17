9a6627ffe1ce64a6dad66a32e1d066483efd4aff
PavanCodes05 <pavancodes05@gmail.com>
2025-04-17 16:49:24 +0530

    Add v1 Commands & Activation Events

--- src/extension.ts
+++ src/extension.ts
@@ -1,4 +1,20 @@
 import * as vscode from 'vscode';
 
-export function activate(context: vscode.ExtensionContext) {};
+export function activate(context: vscode.ExtensionContext) {
+	console.log("NeuroCode is Activated");
+
+	const explain = vscode.commands.registerCommand('neurocode.explain', () => {
+		vscode.window.showInformationMessage("Explain Code Snippet");
+	});
+
+	const refactor = vscode.commands.registerCommand('neurocode.refactor', () => {
+		vscode.window.showInformationMessage("Refactor Code Snippet");
+	});
+
+	const generateDoc = vscode.commands.registerCommand('neurocode.generateDoc', () => {
+		vscode.window.showInformationMessage("Generate Doc");
+	});
+
+	context.subscriptions.push(explain, refactor, generateDoc);
+};
 
