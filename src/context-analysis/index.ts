bb5ddfe52efbc054d4acf6450f63cda9c37d3cd8
PavanCodes05 <pavancodes05@gmail.com>
2025-05-06 19:02:11 +0530

    Context Analysis temp-getProjectStructure

--- src/context-analysis/index.ts
+++ src/context-analysis/index.ts
@@ -1,3 +1,18 @@
+import * as vscode from 'vscode';
+
 import { getProjectStructure } from "./projectStructure";
 
-export { getProjectStructure };
+const getContext = async() => {
+    const rootUri =  vscode.workspace.workspaceFolders?.[0].uri;
+    if(!rootUri) {
+        return null;
+    }
+
+    try {
+        const projectStructure = await getProjectStructure(rootUri);
+    } catch(error) {
+
+    };
+};
+
+export { getContext };
