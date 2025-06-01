21044b90bcca8661bd4742bf8149b338e4b0a205
PavanCodes05 <pavancodes05@gmail.com>
2025-06-01 18:36:38 +0530

    Add LLM Retries

--- src/llm-integration/index.ts
+++ src/llm-integration/index.ts
@@ -6,11 +6,24 @@ import { structurePrompt } from "./structurePrompt";
 const callLLM = async(prompt: string, panel?: vscode.WebviewPanel) => {
     try {
         // Cohere
-        const response = runCohere(prompt, panel);
+        const response = await retryRequests(() => runCohere(prompt, panel));
         return response;
     } catch (error) {
         throw error;
     }
+};
+
+const retryRequests = async<T>(fn: () => Promise<T>, retries = 3, delay = 1000) => {
+    for(let i = 0; i <= retries; i++) {
+        try {
+            console.log("ATTEMPT: ", i + 1);
+            return await fn();
+        } catch (error) {
+            if (i ===  retries) {throw error;};
+            await new Promise(res => setTimeout(res, delay * (i + 1)));
+        }
+    }
+    throw new Error("Failed After Retries!");
 };
 
 export { structurePrompt, callLLM };
